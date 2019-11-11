dc5f76e4bd206886075d40feae7a3c13542dee12
aDavtyan13 <alla.davtyan99@gmail.com>
2019-11-11 18:41:00 +0400

    adding html

--- Social/src/app/app.module.ts
+++ Social/src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +18,9 @@ import { ProfilePageComponent } from './profile-page/profile-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
