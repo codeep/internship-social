fd015b943209ae0ced695896962b4304fbe53ac7
aDavtyan13 <alla.davtyan99@gmail.com>
2019-11-11 16:54:29 +0400

    adding component

--- Social/src/app/app.module.ts
+++ Social/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfilePageComponent
   ],
   imports: [
     BrowserModule,
