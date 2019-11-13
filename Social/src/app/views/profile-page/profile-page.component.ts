71376bd5c7ba311767e7e47574d9692244c3ce44
aDavtyan13 <alla.davtyan99@gmail.com>
2019-11-13 19:05:17 +0400

    adding

--- Social/src/app/views/profile-page/profile-page.component.ts
+++ Social/src/app/views/profile-page/profile-page.component.ts
@@ -6,10 +6,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile-page.component.css']
 })
 export class ProfilePageComponent implements OnInit {
+  hideCoverImage=false;
+  urlCover;
+  urlProfile;
+  hideUploadCoverButton=true;
+  hideUploadProfileButton=true;
+  showUploadCoverButton=false;
+  showUploadProfileButton=false;
+  hideProfileImage=false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  
+  onSelectCoverFile(event) {
+    this.hideUploadCoverButton=false;
+    this.showUploadCoverButton=true;
+    this.hideCoverImage=true;
+
+    console.log('cover');
+
+    if (event.target.files && event.target.files[0]) {
+
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (event) => { 
+        this.urlCover = event.target.result;
+      }
+    }
+  }
+
+  onSelectProfileFile(event) {
+    this.showUploadProfileButton=true;
+    this.hideUploadProfileButton=false;
+    this.hideProfileImage=true;
+
+    if (event.target.files && event.target.files[0]) {
+
+      var reader = new FileReader();
+
+      reader.readAsDataURL(event.target.files[0]);
+
+      reader.onload = (event) => { 
+        this.urlProfile = event.target.result;
+      }
+    }
   }
 
 }
