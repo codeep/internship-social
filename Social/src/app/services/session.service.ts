e664602336fae874bc9d1261e0614f007d2f13d4
laramalkhasyan <larisa0820m@gmail.com>
2019-11-16 18:26:31 +0400

    latest update

--- Social/src/app/services/session.service.ts
+++ Social/src/app/services/session.service.ts
@@ -2,13 +2,20 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SessionService {
     private token;
-    user;
+    user:any;
    getToken() {
        return localStorage.getItem('token');
    }
    setToken(token) {
        localStorage.setItem('token', token);
    }
+
+   getUser() {
+    return JSON.parse(localStorage.getItem('user'));
+    }
+    setUser(user) {
+    localStorage.setItem('user', JSON.stringify(user));
+    }
 
    logout() {
    }
--- Social/src/app/views/feed/feed.component.ts
+++ Social/src/app/views/feed/feed.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RequestService } from 'src/app/services/request-service.service';
 import  '../../endpoints'
 import { Router } from '@angular/router';
+import { SessionService } from 'src/app/services/session.service';
 
 
 @Component({
@@ -10,15 +11,17 @@ import { Router } from '@angular/router';
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent implements OnInit {
+  name:string
 
   constructor(
     private router:Router,
-    
-    ) { 
-    // server.get(POST_NEARBY )
-  }
+    private sessionService:SessionService
+    ) { }
 
   ngOnInit() {
+    debugger
+    this.name = this.sessionService.getUser().firstname + " " +  this.sessionService.getUser().lastname
+    // this.name = this.sessionService.user.firstname + " " + this.sessionService.user.lastname
   }
 
   logout(){
