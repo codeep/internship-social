651c16f1ef0c0aa42380e030fc9340547f3a8dce
KarinAnt <antonyankarinka@gmail.com>
2019-11-18 14:28:56 +0400

    Wisard

--- Social/src/app/views/login/login.component.ts
+++ Social/src/app/views/login/login.component.ts
@@ -45,7 +45,6 @@ export class LoginComponent implements OnInit {
         }else{
           this.errorText = true;
         } 
-      }
       });
     } else {
       if(this.login.controls.password.errors){
@@ -57,7 +56,7 @@ export class LoginComponent implements OnInit {
       this.validateAllFormFields(this.login);
     }
   }
-
+  
   validateAllFormFields(formGroup: FormGroup) {         
     Object.keys(formGroup.controls).forEach(field => {  
       const control = formGroup.get(field);             
